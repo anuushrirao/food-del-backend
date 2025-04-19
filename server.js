2807430b7803172d1a3e20aaa769b0d4a9cd76be
anuushrirao <anushkavshrirao@gmail.com>
2025-04-19 12:11:44 +0530

    file changed for render

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import 'dotenv/config';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+connectDB();
+
+// Routes
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/images", express.static("uploads"));
+
+app.get("/api", (req, res) => {
+  res.send("API Working from Render!");
+});
+
+// Start the server on the port provided by Render
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
